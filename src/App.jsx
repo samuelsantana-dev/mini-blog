773a3814387fe0a53fd38861a4b21eb50726fd22
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-06-25 20:56:54 -0300

    Configuração das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+// Components
+import { Footer } from "./components/Footer.jsx"
+import { NavBar } from "./components/NavBar.jsx"
+// Pages
+import { Home } from "./pages/Home/Home.jsx"
+import { About } from "./pages/About/About.jsx"
 
 function App() {
 
   return (
     <>
-      <p>Mini Blog</p>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+            </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
