49c6401270b291d2210dc6679a1672d0f947cd7f
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-07-01 21:01:30 -0300

    Adicionado rotas e css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { NavBar } from "./components/NavBar.jsx"
 // Pages
 import { Home } from "./pages/Home/Home.jsx"
 import { About } from "./pages/About/About.jsx"
+import { Login } from "./pages/Login/Login.jsx"
+import { Register } from "./pages/Register/Register.jsx"
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/register" element={<Register />}/>
             </Routes>
         </div>
         <Footer />
