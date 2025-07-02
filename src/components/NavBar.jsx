8d3525ae0a22b9cb5ccbd9492c98004c6a77344c
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-07-01 21:00:49 -0300

    Novo login, register e css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom"
-
+import  styles  from "./NavBar.module.css"
 export const NavBar = () => {
+
     return (
-       <nav>
-            <NavLink to="/">
-                mini blog
+       <nav className={styles.navbar}>
+            <NavLink className={styles.brand} to="/">
+                Mini blog
             </NavLink>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
+            <ul className={styles.links_list}>
+                {/*O NavLink vai verificar se a rota esta ativa */}
+                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/">Home</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/about">About</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/login">Login</NavLink></li>
+                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/register">Register</NavLink></li>
             </ul>
        </nav>
     )
