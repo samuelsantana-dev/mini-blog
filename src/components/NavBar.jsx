7d93ec4293ed9efd01531046e990ac4b30efbb66
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-07-08 19:38:54 -0300

    Nova logica para navBar aparecer com if de usuario autentitcado ou nao

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom"
+import { useAuthValue } from "../context/AuthContext"
 import  styles  from "./NavBar.module.css"
 export const NavBar = () => {
-
+    const {user} = useAuthValue(); 
+    console.log('user', user)
     return (
        <nav className={styles.navbar}>
             <NavLink className={styles.brand} to="/">
                 Mini blog
             </NavLink>
             <ul className={styles.links_list}>
-                {/*O NavLink vai verificar se a rota esta ativa */}
                 <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/">Home</NavLink></li>
                 <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/about">About</NavLink></li>
-                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/login">Login</NavLink></li>
-                <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/register">Register</NavLink></li>
+                {!user && (
+                    <>
+                        <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/login">Login</NavLink></li>
+                        <li><NavLink className={({isActive}) => isActive ? styles.active : ""} to="/register">Register</NavLink></li>
+                    </>
+                )}
             </ul>
        </nav>
     )
