346626fed81a3f54014c449529cbc2dee1f47d8c
samuels-space <samuel@00px.com.br>
2025-08-12 21:27:15 -0300

    Ajustes de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
+import styles from "./Footer.module.css";
 export const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <h3>Escreva sobre o que voce quiser</h3>
             <p>2025 - Todos os direitos reservados</p>
         </footer>
